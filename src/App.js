6a45883315d70d963e76aed57d76d2f32269865f
Tapple214 <applegate21441@gmail.com>
2024-10-05 22:28:16 +0800

    useSwipe

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { useState, useEffect } from "react";
 import { useSwipe } from "./components/SwipeDeck";
 
 export default function App() {
-  // Swiping functionality
-  // returned functions = useSwipe({ params })
-
   const items = [
-    { name: "john" },
-    { name: "emma" },
-    { name: "michael" },
-    { name: "sophia" },
-    { name: "william" },
-    { name: "olivia" },
-    { name: "james" },
-    { name: "isabella" },
-    { name: "benjamin" },
-    { name: "mia" },
-    { name: "alexander" },
+    { id: 1, name: "john" },
+    { id: 2, name: "emma" },
+    { id: 3, name: "michael" },
+    { id: 4, name: "sophia" },
+    { id: 5, name: "william" },
+    { id: 6, name: "olivia" },
+    { id: 7, name: "james" },
+    { id: 8, name: "isabella" },
+    { id: 9, name: "benjamin" },
+    { id: 10, name: "mia" },
+    { id: 11, name: "alexander" },
   ];
 
   const {
@@ -30,58 +25,47 @@ export default function App() {
     cardTransform,
     isLoaded,
     cardZIndex,
-  } = useSwipe({
-    items,
-  });
+  } = useSwipe({ items });
 
   return (
     <div className="App">
-      {/* Card */}
       <div
-        className={`tinder ${isLoaded ? "loaded" : ""}`}
+        className={`card-container ${isLoaded ? "loaded" : ""}`}
         ref={swipeContainerRef}
       >
-        <div className="tinder--cards">
+        <div className="cards">
           {noMoreCards ? (
             <p>No more cards</p>
           ) : (
-            <>
-              {items.map((item, index) => (
-                <div
-                  key={index}
-                  className={`card ${
-                    removedCardIds.has(index) ? "removed" : ""
-                  }`}
-                  style={{
-                    transform:
-                      cardTransform && removedCardIds.has(index)
-                        ? cardTransform
-                        : "",
-                    zIndex: cardZIndex[index] || "auto",
-                  }}
-                  ref={(crd) => (allCardsRef.current[index] = crd)}
-                  data-listing-id={index}
-                >
-                  <p>{item.name}</p>
-                </div>
-              ))}
-            </>
+            items.map((item, index) => (
+              <div
+                key={item.id}
+                className={`card ${removedCardIds.has(index) ? "removed" : ""}`}
+                style={{
+                  transform:
+                    cardTransform && removedCardIds.has(index)
+                      ? cardTransform
+                      : "",
+                  zIndex: cardZIndex[index] || "auto",
+                }}
+                ref={(crd) => (allCardsRef.current[index] = crd)}
+                data-index={index}
+              >
+                <p>{item.name}</p>
+              </div>
+            ))
           )}
         </div>
-
-        {/* Buttons */}
-        <div className="tinder--buttons">
+        <div className="button-container">
           <div>
             <button onClick={() => setAction("dislike")} id="dislike">
-              <i className="bi bi-hand-thumbs-down-fill"></i>
+              Dislike
             </button>
-
             <button onClick={() => setAction("want")} id="want">
-              <i className="bi bi-bag-heart-fill"></i>
+              Want
             </button>
-
             <button onClick={() => setAction("like")} id="like">
-              <i className="bi bi-hand-thumbs-up-fill"></i>
+              Like
             </button>
           </div>
         </div>
--- src/components/SwipeDeck.js
+++ src/components/SwipeDeck.js
@@ -1,42 +1,23 @@
 import { useState, useRef, useEffect, useCallback } from "react";
 
 export function useSwipe({ items }) {
-  // NOTE: Cards are not necessarily removed from the original items array
-  // A new set of listing ids of removed cards is just created so they don't
-  // show up again until after the page is refresheds
-
-  // REFERENCES --------------------------------------------------------------
-
-  // Store a reference to the div DOM element that contains the cards
   const swipeContainerRef = useRef(null);
-  // Store a reference to the card DOM element
   const allCardsRef = useRef([]);
 
-  // STATE -------------------------------------------------------------------
-
   const [cardsInitialized, setCardsInitialized] = useState(false);
-  // Action = like, dislike, or want
   const [action, setAction] = useState(null);
-  // Check if there are no more items/cards to display
   const [noMoreCards, setNoMoreCards] = useState(false);
   const [removedCardIds, setRemovedCardIds] = useState(new Set());
-  // Track if the container is loaded
   const [isLoaded, setIsLoaded] = useState(false);
-  // State to trigger a rerender if needed
   const [triggerRerender, setTriggerRerender] = useState(false);
 
-  // STYLING -----------------------------------------------------------------
-
-  // CSS styling states
   const [cardTransform, setCardTransform] = useState("");
   const [cardZIndex, setCardZIndex] = useState({});
 
-  // ACTION HANDLING ---------------------------------------------------------
-
-  const handleAction = async (item, index) => {
+  const handleAction = async (card, index) => {
     const moveOutWidth = document.body.clientWidth * 1.5;
     const moveOutHeight = document.body.clientHeight * 1.5;
-    const direction = 0;
+    let direction = 0;
 
     if (action === "like") {
       direction = moveOutWidth;
@@ -47,89 +28,64 @@ export function useSwipe({ items }) {
     }
     const rotation = action === "like" ? 30 : -30;
 
-    // Mark the card as "removed"
-    setRemovedCardIds((prev) => new Set(prev).add(index));
     setCardTransform(`translate(${direction}px, 0px) rotate(${rotation}deg)`);
 
-    try {
-      await fetch(`/api/listing-feedback/${index}`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ feedback: action }),
-      });
-    } catch (error) {
-      console.error("Failed to send feedback");
-    }
-
-    // Add the card to the removed set and check if there are no more cards
-    // Compare the set and ref to set "No more cards" if they match
+    // Mark the card as "removed"
     setRemovedCardIds((prev) => {
-      const newSet = new Set(prev).add(index);
+      const newSet = new Set(prev);
+      newSet.add(index);
       if (newSet.size === allCardsRef.current.length) {
         setNoMoreCards(true);
       }
       return newSet;
     });
 
-    // Reset action to prevent continuous updates
     setAction(null);
   };
 
-  // Use useCallback to ensure handleSwipe is stable
-  // To prevent unnecessary re-renders
   const handleSwipe = useCallback(async () => {
-    // Check if there are no items
     if (!action || items.length === 0) return;
 
-    // Find the first card that has not been removed
     const card = allCardsRef.current.find(
       (c) => c && !removedCardIds.has(c.dataset.index)
     );
     if (!card) return;
 
-    const listing = items.find((items) => listing.id === card.dataset.index);
+    const item = items[card.dataset.index];
 
-    if (listing) {
-      await handleAction(card, listing);
+    if (item) {
+      await handleAction(card, card.dataset.index);
     }
   }, [action, items]);
 
-  // UseEffect 1: Handles swiping when cards are initialized and an action is set.
   useEffect(() => {
     if (cardsInitialized && action) {
       handleSwipe();
     }
   }, [cardsInitialized, action, handleSwipe]);
 
-  // UseEffect 2: Reinitializes cards when items change or component re-renders.
   useEffect(() => {
     if (cardsInitialized) return;
 
-    // Check if there are no items
-    if (items?.length === 0) {
+    if (items.length === 0) {
       setNoMoreCards(true);
       return;
     }
 
-    // Check if allCardsRef.current is empty, trigger rerender
     if (!allCardsRef.current || allCardsRef.current.length === 0) {
-      // Re-render because allCardsRef is empty
       setTriggerRerender((prev) => !prev);
       return;
     }
 
-    // Filter out cards that have been removed
     const newCards = allCardsRef.current.filter(
       (card) => card && !removedCardIds.has(card.dataset.index)
     );
 
-    // If no cards are left, mark noMoreCards as true
     if (newCards.length === 0) {
       setNoMoreCards(true);
       return;
     }
 
-    // Set Z-index for each card to stack them correctly
     newCards.forEach((card, index) => {
       if (card) {
         setCardZIndex((prevZIndex) => ({
@@ -139,12 +95,10 @@ export function useSwipe({ items }) {
       }
     });
 
-    // Mark that the cards have been successfully loaded
     setIsLoaded(true);
     setCardsInitialized(true);
   }, [items, removedCardIds, triggerRerender]);
 
-  // UseEffect 3: Ensures that allCardsRef.current.length is not empty
   useEffect(() => {
     if (!isLoaded && allCardsRef.current.length > 0) {
       setIsLoaded(true);
